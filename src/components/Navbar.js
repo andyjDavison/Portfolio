ee63cb986567b4c04579114c9000c27c21923e36
andyjDavison <andyd.soccer@gmail.com>
2024-07-30 21:45:15 -0400

    Update Navbar style and layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,11 +26,15 @@ function Navbar() {
     return(
         // <nav id="nav" style={{"top": `${show ? 'active' : 'hidden'}`}}>
         <nav id="nav">
-            <a href="#home-page" className="nav-item name">ANDREW DAVISON</a>
-            <a href="#education-page" className="nav-item">EDUCATION</a>
-            <a href="#projects-page" className="nav-item">PROJECTS</a>
-            <a href="#about-page" className="nav-item">ABOUT</a>
-            <img src="/assets/img_1.jpg" alt="" className="nav-img"/>
+          <div id="nav--left">
+            <a href="#home-page" className="nav--item">ANDREW DAVISON</a>
+            <img src="/assets/img_1.jpg" alt="" id="nav--img"/>
+          </div>
+          <div id="nav--right">
+            <a href="#about-page" className="nav--item">ABOUT</a>
+            <a href="#projects-page" className="nav--item">PROJECTS</a>
+            <a href="#education-page" className="nav--item">EDUCATION</a>
+          </div>  
         </nav>
     )
 }
